c167e76c12a20a906b346e47016003d6806f0725
彭津雲 <pxc1861@hotmail.com>
2019-04-09 22:30:53 +0800

    0_0

--- Playground/Branch01.js
+++ Playground/Branch01.js
@@ -17,9 +17,10 @@ class RanLife {
 
         this.growth_speed = speed;
         this.line_weight = weight;
+        this.hidden_first_lines_movements = false;
 
         this.step = step;
-        this.branch_angle_base = int(random(0, 2)) ? 90 : -90;
+        this.branch_angle_base = int(random(0, 2)) ? 45 : -45;
         this.branch_angle_step = int(random(0, 2)) ? step[0] : step[1];
 
         this.stroke_color = (stroke);
@@ -38,6 +39,8 @@ class RanLife {
 
             this.growth_base = 0;
 
+            this.hidden_first_lines_movements = false;
+
             if (true) {
                 this.growth_direction = p5.Vector.fromAngle(radians(this.branch_angle_base, 1));
 
@@ -51,7 +54,16 @@ class RanLife {
     }
 
     update() {
-        stroke(this.stroke_color,0,0);
+
+        if (this.hidden_first_lines_movements)
+        {
+            stroke(0);
+        } else {
+            stroke(this.stroke_color,0,0);
+        }
+        
+
+
         strokeWeight(this.line_weight)
 
         line(this.location.x, this.location.y, this.location.x + this.growth_direction.x * this.growth_speed *this.growth_base, this.location.y + this.growth_direction.y * this.growth_speed * this.growth_base);
@@ -87,13 +99,14 @@ function setup() {
 
     simple = new RanLife();
 
-    let n = 8;
+    let n = 9;
+    let ran_start = random(0,90);
     for (let i = 0; i < n; i++ ) {
         // As always, this generate a beautiful pattern.
         // Add a `step` parameter to control the line rotation
         // line speed grows with its weight 
         // the base color was kept at 5, really dark, which makes the ADD blendMode works well.
-        complex.push(new RanLife(0 + 90 * i,(i+1)*2,5,i*.2,[45,-45]));
+        complex.push(new RanLife(0 + 45 * i,(i+1)*1.5,2.5,i*.3,[45,-45]));
     }
 }
 
